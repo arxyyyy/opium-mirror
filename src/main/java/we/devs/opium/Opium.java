89ad5ab1d703ee90b46ec3249f4208a2209b97d8
swagmessiah1205 <158429633+swagmessiah1205@users.noreply.github.com>
2025-01-04 09:50:23 -0800

    dadada

--- src/main/java/we/devs/opium/Opium.java
+++ src/main/java/we/devs/opium/Opium.java
@@ -22,7 +22,7 @@ import we.devs.opium.api.manager.miscellaneous.FontManager;
 import java.awt.*;
 
 public class Opium implements ModInitializer {
-    public static final String NAME = "Opium";
+    public static final String NAME = "0piumh4ck.cc";
     public static final String VERSION = "1.3.6-Beta";
     public static final Logger LOGGER = LoggerFactory.getLogger("Opium");
 
