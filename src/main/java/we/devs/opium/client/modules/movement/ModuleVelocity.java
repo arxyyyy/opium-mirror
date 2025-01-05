8ab2e683b8841940edc25516aab451020c982bdf
swagmessiah1205 <158429633+swagmessiah1205@users.noreply.github.com>
2025-01-04 21:07:50 -0800

    grim velocity started

--- src/main/java/we/devs/opium/client/modules/movement/ModuleVelocity.java
+++ src/main/java/we/devs/opium/client/modules/movement/ModuleVelocity.java
@@ -1,29 +1,42 @@
 package we.devs.opium.client.modules.movement;
 
+import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
+import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
+import net.minecraft.util.Formatting;
 import we.devs.opium.api.manager.module.Module;
 import we.devs.opium.api.manager.module.RegisterModule;
+import we.devs.opium.asm.mixins.IClientPlayerEntity;
 import we.devs.opium.asm.mixins.IEntityVelocityUpdateS2CPacket;
 import we.devs.opium.asm.mixins.IExplosionS2CPacket;
 import we.devs.opium.client.events.EventPacketReceive;
 import we.devs.opium.client.events.EventPush;
 import we.devs.opium.client.values.impl.ValueBoolean;
 import we.devs.opium.client.values.impl.ValueNumber;
-import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
-import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
-import net.minecraft.util.Formatting;
 
-@RegisterModule(name="Velocity", description="Remove the knockback of the player.", category=Module.Category.MOVEMENT)
+@RegisterModule(name="Velocity", description="Remove the knockback of the player.", category = Module.Category.MOVEMENT )
 public class ModuleVelocity extends Module {
     public static ValueBoolean noPush = new ValueBoolean("NoPush", "NoPush", "", false);
+    public static ValueBoolean grim = new ValueBoolean("GrimAC", "GrimAC", "Bypass for GrimAC v2", false);
     public static ValueNumber horizontal = new ValueNumber("Horizontal", "Horizontal", "", 0.0f, 0.0f, 100.0f);
     public static ValueNumber vertical = new ValueNumber("Vertical", "Vertical", "", 0.0f, 0.0f, 100.0f);
 
-    @Override
     public void onPacketReceive(EventPacketReceive event) {
         EntityVelocityUpdateS2CPacket sPacketEntityVelocity;
+
         if (mc.player == null || mc.world == null) {
             return;
         }
+
+        if (grim.getValue() && event.getPacket() instanceof EntityVelocityUpdateS2CPacket) {
+            mc.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.Full(mc.player.getX(), mc.player.getY(), mc.player.getZ(), ((IClientPlayerEntity) mc.player).getLastYaw(), ((IClientPlayerEntity) mc.player).getLastPitch(), mc.player.isOnGround()));
+            mc.getNetworkHandler().sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.STOP_DESTROY_BLOCK, BlockPos.ofFloored(mc.player.getPos()), Direction.DOWN));
+            return;
+        }
+
         if (event.getPacket() instanceof EntityVelocityUpdateS2CPacket && (sPacketEntityVelocity = (EntityVelocityUpdateS2CPacket) event.getPacket()).getEntityId() == mc.player.getId()) {
             if (horizontal.getValue().floatValue() == 0.0f && vertical.getValue().floatValue() == 0.0f) {
                 event.cancel();
@@ -44,14 +57,12 @@ public class ModuleVelocity extends Module {
         }
     }
 
-    @Override
     public void onPush(EventPush event) {
         if (noPush.getValue()) {
             event.cancel();
         }
     }
 
-    @Override
     public String getHudInfo() {
         return "H" + horizontal.getValue().floatValue() + "%" + Formatting.GRAY + "," + Formatting.WHITE + "V" + vertical.getValue().floatValue() + "%";
     }
