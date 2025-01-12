98c619ac2511aed2f3961c0e2bf3500ae3899eb9
Dreizz <brumbaal98@gmail.com>
2025-01-12 10:43:17 +0100

    Update and rename ModuleFOV.java to ModuleZoom.java

--- src/main/java/we/devs/opium/client/modules/visuals/ModuleZoom.java
+++ src/main/java/we/devs/opium/client/modules/visuals/ModuleZoom.java
@@ -4,9 +4,9 @@ import we.devs.opium.api.manager.module.Module;
 import we.devs.opium.api.manager.module.RegisterModule;
 import we.devs.opium.client.values.impl.ValueNumber;
 
-@RegisterModule(name = "FOV", description = "Customize your field of range.", category = Module.Category.VISUALS)
+@RegisterModule(name = "Zoom", description = "Zooms lol.", category = Module.Category.VISUALS)
 public class ModuleFOV extends Module {
-    private final ValueNumber fovSetting = new ValueNumber("Amount", "Amount", "", 90, 30, 110);
+    private final ValueNumber fovSetting = new ValueNumber("Amount", "Amount", "", 30, 10, 130);
 
 
     private int oldFov;
