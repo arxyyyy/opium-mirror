a2693ffa38b150f69fbab96b841991918873abff
AnonCx <callmemika1805@gmail.com>
2025-01-13 00:37:37 +0100

    Pushin P

--- src/main/java/we/devs/opium/client/modules/player/ModuleAntiCrawl.java
+++ src/main/java/we/devs/opium/client/modules/player/ModuleAntiCrawl.java
@@ -7,6 +7,7 @@ import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.util.math.Direction;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket.Action;
 import we.devs.opium.api.utilities.BlockUtils;
+import we.devs.opium.api.utilities.ChatUtils;
 import we.devs.opium.api.utilities.HoleUtils;
 import we.devs.opium.api.manager.module.Module;
 import we.devs.opium.api.manager.module.RegisterModule;
@@ -19,6 +20,12 @@ public class ModuleAntiCrawl extends Module {
     double progress = 0;
 
     @Override
+    public void onEnable() {
+        super.onEnable();
+        progress = 0;
+    }
+
+    @Override
     public void onTick() {
         super.onTick();
         if (super.nullCheck()) {
@@ -27,16 +34,21 @@ public class ModuleAntiCrawl extends Module {
         int slot = InventoryUtils.findItem(Items.NETHERITE_PICKAXE, 0, 8);
         if (slot != -1) {
             BlockPos pos = new BlockPos((int) Math.floor(mc.player.getX()), (int) mc.player.getY(), (int) Math.floor(mc.player.getZ()));
-            BlockState state = mc.world.getBlockState(pos);
+            BlockState state = mc.world.getBlockState(pos.up());
             progress += BlockUtils.getBreakDelta(slot, state);
-
+            //ChatUtils.sendMessage("Pickaxe found");
             if (HoleUtils.isInCrawlHole(mc.player)) {
+                //ChatUtils.sendMessage("In Hole");
                 if ((BlockUtils.getBlockResistance(pos.up()) == BlockUtils.BlockResistance.Breakable || BlockUtils.getBlockResistance(pos.up()) == BlockUtils.BlockResistance.Resistant) || (BlockUtils.getBlockResistance(pos.down()) == BlockUtils.BlockResistance.Breakable || BlockUtils.getBlockResistance(pos.down()) == BlockUtils.BlockResistance.Resistant)) {
-                    if (progress >= 0.0) {
-                        mc.player.networkHandler.sendPacket(new PlayerActionC2SPacket(Action.START_DESTROY_BLOCK, pos, Direction.UP));
+                    ChatUtils.sendMessage("Breaking " + progress);
+                    if (progress <= 0.01) {
+                        ChatUtils.sendMessage("Breaking Started");
+                        mc.player.networkHandler.sendPacket(new PlayerActionC2SPacket(Action.START_DESTROY_BLOCK, pos.up(), Direction.UP));
                     }
-                    if (progress >= 1.0) {
-                        mc.player.networkHandler.sendPacket(new PlayerActionC2SPacket(Action.STOP_DESTROY_BLOCK, pos, Direction.UP));
+                    if (progress > 1.0) {
+                        ChatUtils.sendMessage("Breaking Finished");
+                        InventoryUtils.switchSlot(slot, true);
+                        mc.player.networkHandler.sendPacket(new PlayerActionC2SPacket(Action.STOP_DESTROY_BLOCK, pos.up(), Direction.UP));
                         progress = 0;
                     }
                 } else if (BlockUtils.getBlockResistance(pos) == BlockUtils.BlockResistance.Unbreakable) {
