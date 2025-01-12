8ea316e4fe59bda2e1a761f2487a21a8a939a8ca
swagmessiah1205 <158429633+swagmessiah1205@users.noreply.github.com>
2025-01-12 01:48:04 -0800

    1.3.8

--- src/main/java/we/devs/opium/Opium.java
+++ src/main/java/we/devs/opium/Opium.java
@@ -25,7 +25,7 @@ import java.util.TimerTask;
 
 public class Opium implements ModInitializer {
     public static final String NAME = "0piumh4ck.cc";
-    public static final String VERSION = "1.3.8-Beta";
+    public static final String VERSION = "1.3.8-beta";
     public static final Logger LOGGER = LoggerFactory.getLogger("Opium");
 
     public static Color COLOR_CLIPBOARD;
