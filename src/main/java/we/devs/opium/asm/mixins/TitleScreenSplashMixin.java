2522d12860f42d92fec27deef999be715f0fa403
AnonCx <callmemika1805@gmail.com>
2025-01-12 15:08:53 +0100

    type shit

---
+++ src/main/java/we/devs/opium/asm/mixins/TitleScreenSplashMixin.java
@@ -0,0 +1,30 @@
+package we.devs.opium.asm.mixins;
+
+import net.minecraft.client.gui.screen.SplashTextRenderer;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.Random;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenSplashMixin {
+
+    @Shadow
+    private SplashTextRenderer splashText;
+
+    @Inject(method = "init", at = @At("HEAD"))
+    private void onInit(CallbackInfo info) {
+        Random random = new Random();
+        String[] customSplashes = {
+                "Cxiy",
+                "Heedi",
+                "Idk Any Other Devs",
+                "Opium",
+                "" // Empty cz looks good ngl
+        };
+        splashText = new SplashTextRenderer(customSplashes[random.nextInt(customSplashes.length)]);
+    }
+}
