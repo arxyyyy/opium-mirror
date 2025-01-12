9fd3b77fda4c8421485b652904b0df8bf8d9c459
AnonCx <callmemika1805@gmail.com>
2025-01-12 23:49:47 +0100

    Discord RPC

--- src/main/java/we/devs/opium/api/manager/module/ModuleManager.java
+++ src/main/java/we/devs/opium/api/manager/module/ModuleManager.java
@@ -38,6 +38,7 @@ public class ModuleManager implements IMinecraft, EventListener {
         this.register(new ModuleHUDEditor());
         this.register(new ModuleParticles());
         this.register(new ModuleRotations());
+        this.register(new ModuleRPC());
 
         //Combat
         this.register(new ModuleAutoArmor());
