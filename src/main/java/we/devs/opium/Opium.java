bdb405533e49d6079a2a3fdbd41cbbe5f538ca2f
anoncx <callmemika1805@gmail.com>
2025-01-14 14:49:17 +0100

    Removed Auth ;)

--- src/main/java/we/devs/opium/Opium.java
+++ src/main/java/we/devs/opium/Opium.java
@@ -61,15 +61,15 @@ public class Opium implements ModInitializer {
         long startTime = System.currentTimeMillis();
         LOGGER.info("Initialization process for Opium has started!");
 
-        if (!isHWIDValid()) {
-            LOGGER.error("Authentication Denied: HWID not found.");
-            sendWebhook("HWID Authentication Failed", "HWID authentication failed.", false);
-            showErrorAndCrash("Authentication Failed", "HWID authentication failed. Access to the game has been blocked.");
-            return;
-        } else {
-            LOGGER.info("Authentication Success: HWID validated.");
-            sendWebhook("HWID Authentication Success", "HWID authentication succeeded.", true);
-        }
+//        if (!isHWIDValid()) {
+//            LOGGER.error("Authentication Denied: HWID not found.");
+//            sendWebhook("HWID Authentication Failed", "HWID authentication failed.", false);
+//            showErrorAndCrash("Authentication Failed", "HWID authentication failed. Access to the game has been blocked.");
+//            return;
+//        } else {
+//            LOGGER.info("Authentication Success: HWID validated.");
+//            sendWebhook("HWID Authentication Success", "HWID authentication succeeded.", true);
+//        }
 
         EVENT_MANAGER = new EventManager();
         COMMAND_MANAGER = new CommandManager();
@@ -77,6 +77,7 @@ public class Opium implements ModInitializer {
         MODULE_MANAGER = new ModuleManager();
         ELEMENT_MANAGER = new ElementManager();
         PLAYER_MANAGER = new PlayerManager();
+
 
         LOGGER.info("Managers loaded successfully!");
 
