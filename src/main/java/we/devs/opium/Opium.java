6f3ff1ce796d46d69bd7e68f9c0b532753471d90
AnonCx <callmemika1805@gmail.com>
2025-01-15 00:26:48 +0100

    - Changed Opium to Version 1.4.1

--- src/main/java/we/devs/opium/Opium.java
+++ src/main/java/we/devs/opium/Opium.java
@@ -35,7 +35,7 @@ import java.util.TimerTask;
 public class Opium implements ModInitializer {
 
     public static final String NAME = "0piumh4ck.cc";
-    public static final String VERSION = "1.4.0-beta";
+    public static final String VERSION = "1.4.1-beta";
     public static final Logger LOGGER = LoggerFactory.getLogger("Opium");
 
     private static final String HWID_LIST_URL = "https://raw.githubusercontent.com/heeedii/Opium-Hwid/refs/heads/main/hwid-list";
