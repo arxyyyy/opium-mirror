d1f01ead3fc66db9fade28fb50dc99bd65b498a4
qweru <>
2025-01-15 18:42:17 +0200

    capes

---
+++ src/main/java/we/devs/opium/asm/mixins/AbstractClientPlayerEntityMixin.java
@@ -0,0 +1,39 @@
+package we.devs.opium.asm.mixins;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.client.util.SkinTextures;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import we.devs.opium.api.manager.miscellaneous.UUIDManager;
+
+import static we.devs.opium.api.utilities.IMinecraft.mc;
+
+@Mixin(AbstractClientPlayerEntity.class)
+public abstract class AbstractClientPlayerEntityMixin {
+
+    @Shadow @Nullable protected abstract PlayerListEntry getPlayerListEntry();
+
+    @Unique
+    private static @NotNull SkinTextures getModifiedSkinTexture(SkinTextures original, Identifier newCape) {
+        return new SkinTextures(original.texture(), original.textureUrl(), newCape, original.elytraTexture(), original.model(), original.secure());
+    }
+
+    @Unique
+    private static final Identifier CAPE = Identifier.of("opium", "textures/capes/custom_cape.png");
+
+    @ModifyReturnValue(method = "getSkinTextures", at = @At(value = "RETURN"))
+    SkinTextures getSkinTextures(SkinTextures original) {
+        if(this.getPlayerListEntry() != null && (UUIDManager.hasCustomCape(this.getPlayerListEntry().getProfile().getId()) || this.equals(mc.player))) {
+            return getModifiedSkinTexture(original, CAPE);
+        }
+        return original;
+    }
+
+}
