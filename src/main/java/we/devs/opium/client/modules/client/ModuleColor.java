6cd80a2910a5a5caa74ffff23f0753f8777894ca
AnonCx <callmemika1805@gmail.com>
2025-01-16 17:49:49 +0100

    - Made The Client GUI Opium Theme

--- src/main/java/we/devs/opium/client/modules/client/ModuleColor.java
+++ src/main/java/we/devs/opium/client/modules/client/ModuleColor.java
@@ -11,7 +11,7 @@ import java.awt.*;
 @RegisterModule(name="Color", description="Manages the client's global color.", category=Module.Category.CLIENT, persistent=true)
 public class ModuleColor extends Module {
     public static ModuleColor INSTANCE;
-    public final ValueColor color = new ValueColor("Color", "Color", "The client's global color.", new Color(255, 0, 0,255));
+    public final ValueColor color = new ValueColor("Color", "Color", "The client's global color.", new Color(97, 97, 97,255));
     ValueCategory rainbowCategory = new ValueCategory("Rainbow", "Manage rainbow");
     public ValueNumber rainbowOffset = new ValueNumber("RainbowOffset", "Offset", "", this.rainbowCategory, 255, 0, 1000);
     public ValueNumber rainbowSat = new ValueNumber("RainbowSaturation", "Saturation", "", this.rainbowCategory, 255, 0, 255);
