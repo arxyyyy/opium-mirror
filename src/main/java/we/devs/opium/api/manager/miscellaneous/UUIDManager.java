11b9791b6af695498a754d9e5032a0bc75a0ea0d
anoncx <108609556+anoncx@users.noreply.github.com>
2025-01-16 11:17:59 +0100

    - Added VoidMatters UUID to Manager

--- src/main/java/we/devs/opium/api/manager/miscellaneous/UUIDManager.java
+++ src/main/java/we/devs/opium/api/manager/miscellaneous/UUIDManager.java
@@ -8,7 +8,7 @@ public class UUIDManager {
 
     // Änderbare Liste der Spieler-UUIDs, denen das Cape angezeigt werden soll
     public static final List<UUID> ALLOWED_UUIDS = new ArrayList<>(List.of(
-            UUID.fromString("11111111-1111-1111-1111-111111111111"), // Beispiel-UUID 1
+            UUID.fromString("31350a69-f9d9-4a55-bd9f-e037eb8d64e5"), // VoidMatter
             UUID.fromString("22222222-2222-2222-2222-222222222222")  // Beispiel-UUID 2
     ));
 
